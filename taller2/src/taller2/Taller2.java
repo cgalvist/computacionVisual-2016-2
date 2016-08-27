9ba4e4866374d663726beedd195e8139b78aca58
Jarts301 <rahamub301@gmail.com>
2016-08-26 21:11:52 -0500

    Taller 2 con funcionalidad completa.

--- taller2/src/taller2/Taller2.java
+++ taller2/src/taller2/Taller2.java
@@ -1,75 +1,89 @@
 package taller2;
 
+import java.awt.Point;
 import processing.core.PApplet;
 import processing.core.PImage;
 import processing.event.MouseEvent;
 
-public class Taller2 extends PApplet{
-    
-    PImage img, zoom;
-    
-    int escala = 5;
-    int ancho = 900, alto = 700;
-    int anchoZoom = (ancho / escala), altoZoom = (alto / escala);
-    float numZoom = 1.0f, rotacion = 0;
-    
-    @Override
-    public void settings(){
-        size(ancho,alto);
-    }
+public class Taller2 extends PApplet {
 
-    @Override
-    public void setup(){
-        img = loadImage("mapa.jpg");
-        zoom = loadImage("mapa.jpg");
-    }
-    
-    @Override
-    public void draw() {
-        background(0);
-        
-        rotate(rotacion);
-        scale(numZoom);
-        image(zoom, 0, 0, ancho, alto);
-        
-        rotate(-rotacion);
-        scale(1 / numZoom);
-        image(img, 0, 0, anchoZoom, altoZoom);
-        
-        if(mouseX > anchoZoom || mouseY > altoZoom || mouseX < 0 || mouseY < 0){
-            numZoom = 1;
-            rotacion = 0;
-        } else {
-            zoom = img.get(mouseX * (escala), mouseY * (escala),ancho,alto);
-        }
-    }
-    
-    @Override
-    public void mousePressed(MouseEvent event) {
-        if(mouseButton == LEFT){
-            rotacion += PI / 16.0f;
-        }
-        if(mouseButton == RIGHT){
-            rotacion += - PI / 16.0f;
-        }
-    }
-    
-    @Override
-    public void mouseWheel(MouseEvent event) {
-        if(numZoom > 0.05f){
-            if(event.getCount() == -1){
-                numZoom += 0.05f;
-            } else {
-                numZoom -= 0.05f;
-            }
-        } else {
-            numZoom = 0.051f;
-        }
-    }
-    
-    public static void main(String[] args) {
-        String name = Taller2.class.getName();
-        PApplet.main(name);
-    }
-    
+	PImage img;
+	PImage zoomImage;
+	Float escalaRect;
+	Float rotationRect;
+
+	Point posicion;
+
+	@Override
+	public void settings() {
+		size(800, 800);
+	}
+
+	@Override
+	public void setup() {
+		img = loadImage("mapa.jpg");
+		posicion = new Point(0, 0);
+		escalaRect = 1.0f;
+		rotationRect = 0.0f;
+	}
+
+	@Override
+	public void draw() {
+		background(0);
+		pushMatrix();
+		translate(width/2, height/2);
+		rotate(radians(rotationRect));
+		zoomImage = img.get((int) posicion.getX(), (int) posicion.getY(), (int) ((40 * escalaRect) * 7),
+				(int) ((40 * escalaRect) * 7));
+		image(zoomImage, -width/2, -height/2, 800, 800);
+		popMatrix();
+
+		image(img, 0, 0, img.width / 7, img.height / 7);
+
+		if ((mouseX < (img.width / 7) - (20 * escalaRect) && mouseX > 0 + (20 * escalaRect))
+				&& (mouseY > 0 + (20 * escalaRect) && mouseY < (img.height / 7) - (20 * escalaRect))) {
+
+			posicion.setLocation((mouseX - (20 * escalaRect)) * 7, (mouseY - (20 * escalaRect)) * 7);
+			
+			pushMatrix();
+			fill(255, 255, 255, 150);
+			translate(mouseX, mouseY);
+			rotate(radians(rotationRect));
+			rect(-20 * escalaRect, -20 * escalaRect, (40 * escalaRect), 40 * escalaRect);
+			popMatrix();
+
+		}
+
+	}
+
+	@Override
+	public void mousePressed() {
+
+		if (mouseButton == LEFT) {
+			rotationRect=rotationRect+5.0f;
+		} else if (mouseButton == RIGHT) {
+			rotationRect=rotationRect-5.0f;
+		}
+
+	}
+
+	@Override
+	public void mouseWheel(MouseEvent event) {
+		if ((mouseX < (img.width / 7) - (20 * escalaRect) && mouseX > 0 + (20 * escalaRect))
+				&& (mouseY > 0 + (20 * escalaRect) && mouseY < (img.height / 7) - (20 * escalaRect))) {
+
+			if (event.getCount() < 0 && (escalaRect * 40) < (img.height / 7) - 50) {
+				escalaRect = escalaRect + 0.1f;
+			} else if (event.getCount() > 0 && escalaRect > 0.2) {
+				escalaRect = escalaRect - 0.1f;
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		String name = Taller2.class.getName();
+		PApplet.main(name);
+	}
+
 }
